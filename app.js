fac3197216dd393bdb890e59e019bc629fa0363d
Dorelly Crisanto <dorelly.crisanto@gmail.com>
2019-08-14 11:50:38 -0500

    backend login - validación de email y password

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json())
 // Importar rutas
 var appRoutes = require('./routes/app');
 var userRoutes = require('./routes/user');
+var loginRoutes = require('./routes/login');
 
 // Establecer la conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) => {
@@ -35,6 +36,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) 
 // Declarar Mildeware: se ejecuta antes de que se resuelvan otras rutas
 // Cuando cualquier petición haga math con la pleca se usará appRooutes
 app.use('/user', userRoutes); // Delante porque sino siempre pasaría por la ruta de abajo
+app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
 
---
+++ routes/login.js
@@ -0,0 +1,55 @@
+var express = require('express');
+// Para hacer maps de la contraseña que se ingresa con la que se encuentra en la base de datos
+var bcrypt = require('bcryptjs');
+
+var app = express();
+
+var Usuario = require('../models/user');
+
+app.post('/', (req, res) => {
+    var body = req.body;
+    // Verificando que exista un usuario con el correo electrónico
+    // la condición de búsqueda: mientras que el email sea = a body.email
+    Usuario.findOne({ email: body.email }, (err, userDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                message: 'Error al buscar usuarios',
+                errors: err
+            })
+        }
+        // Evaluar si existe un usuario en la base de datos (userDB) con el email indicado
+        if (!userDB) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Credenciales incorrectas - email', // - email: es para saber en que punto está fallando
+                errors: err
+            });
+        }
+
+        // Para verificar la contraseña: nos permite tomar un string que queremos validar con otro string
+        // body.password: lo que está escribiendo el usuario y lo comparo con el
+        // userDB de la base de datos y regresa un valor booleano
+        if (!bcrypt.compareSync(body.password, userDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Credenciales incorrectas - password',
+                errors: err
+            });
+        }
+
+        // Crear un token, ya estamos en el caso que el correo y contraseña son válidos
+
+        res.status(200).json({
+            ok: true,
+            message: 'Login',
+            user: userDB,
+            id: userDB._id
+        });
+    });
+
+
+});
+
+module.exports = app;
